fcf1db6ae12a80fea3e49ba18f7198a3fef5dd9b
LeeCliff254 <147739214+LeeCliff254@users.noreply.github.com>
2024-06-20 18:41:06 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,29 +1,14 @@
-const taskList = document.getElementById('task-list');
 
-function addTask() {
-  const newTask = document.getElementById('new-task').value;
-  if (newTask) {
-    const listItem = document.createElement('li');
-    listItem.textContent = newTask;
+function display(value) {
+  document.getElementById("result").value += value;
+}
 
-    const completeButton = document.createElement('span');
-    completeButton.classList.add('complete-task');
-    completeButton.textContent = '✔';
-    completeButton.addEventListener('click', function() {
-      listItem.classList.toggle('completed');
-    });
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
 
-    const deleteButton = document.createElement('span');
-    deleteButton.classList.add('delete-task');
-    deleteButton.textContent = '❌';
-    deleteButton.addEventListener('click', function() {
-      taskList.removeChild(listItem);
-    });
-
-    listItem.appendChild(completeButton);
-    listItem.appendChild(deleteButton);
-
-    taskList.appendChild(listItem);
-    document.getElementById('new-task').value = '';
-  }
+function calculate() {
+  var p = document.getElementById("result").value;
+  var q = eval(p);
+  document.getElementById("result").value = q;
 }
